fa151fa793e7144272fdcfec23d2d3cbd4865875
navarathan <navarathan0926@gmail.com>
2025-02-02 16:52:38 +0530

    #3, Implement Login REST API with JWT Token

---
+++ server/src/main/java/com/example/server/repo/UserRepo.java
@@ -0,0 +1,11 @@
+package com.example.server.repo;
+
+import com.example.server.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, Integer> {
+
+    Users findByUsername(String username);
+}
